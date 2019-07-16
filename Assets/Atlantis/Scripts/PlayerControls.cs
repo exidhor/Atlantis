67d0c32f751393369018eb60f86d53da7a12886c
Exhidor <jipay.detiege@gmail.com>
2019-07-16 13:28:05 +0200

    add player controls (wip)

---
+++ Assets/Atlantis/Scripts/PlayerControls.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControls : MonoBehaviour
+{
+    [Header("Speed controls")]
+    [SerializeField] float _acceleration;
+    [SerializeField] float _startSpeed;
+    [SerializeField] float _maxSpeed;
+
+    [SerializeField] float _angularSpeed;
+
+    [Header("Linking")]
+    [SerializeField] PlayerInputFeedback _feedback;
+    [SerializeField] PlayerShip _ship;
+    [SerializeField] PlayerCamera _camera;
+
+    bool _shipInputStarted;
+    Vector2 _originScreenPoint;
+
+    void Update()
+    {
+        if(!_shipInputStarted
+            && Input.GetMouseButtonDown(0))
+        {
+            _shipInputStarted = true;
+            _originScreenPoint = Input.mousePosition;
+        }
+
+
+    }
+}
