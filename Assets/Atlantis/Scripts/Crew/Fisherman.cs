25b15f99ad206223df29a8fdb3bab001ae6640b9
Exhidor <jipay.detiege@gmail.com>
2019-07-16 22:16:19 +0200

    add fish

---
+++ Assets/Atlantis/Scripts/Crew/Fisherman.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fisherman : Crew
+{
+    [SerializeField] GameObject _floatModel;
+
+    FishZone _fishZone;
+
+    void OnEnable()
+    {
+        _floatModel.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerType.instance.layerFish)
+        {
+            _fishZone = other.gameObject.GetComponent<FishZone>();
+            _floatModel.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (_fishZone != null
+            && _fishZone.gameObject == other.gameObject)
+        {
+            _fishZone = null;
+            _floatModel.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if(_fishZone != null)
+        {
+            Fish(Time.deltaTime);
+        }
+    }
+
+    void Fish(float dt)
+    {
+        // todo
+    }
+}
---
+++ Assets/Atlantis/Scripts/LayerType.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using Tools;
+
+public class LayerType : MonoSingleton<LayerType>
+{
+    public int layerFish
+    {
+        get { return _layerFish; }
+    }
+
+    [SerializeField] int _layerFish;
+
+}
