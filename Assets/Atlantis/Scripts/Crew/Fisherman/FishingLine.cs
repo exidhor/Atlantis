736c2ccfe06e7fb5ec148306ad09ac1469671b01
Exhidor <jipay.detiege@gmail.com>
2019-07-18 18:26:41 +0200

    work on stop fish anim (wip)

--- Assets/Atlantis/Scripts/Crew/Fisherman/FishingLine.cs
+++ Assets/Atlantis/Scripts/Crew/Fisherman/FishingLine.cs
@@ -1,16 +1,45 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
 
 [RequireComponent(typeof(LineRenderer))]
 public class FishingLine : MonoBehaviour
 {
-    [Header("Animation")]
-    [SerializeField] float _duration;
-    [SerializeField] string _distEndSpeedCurveName;
-    [SerializeField] string _zEndSpeedCurveName;
+    [Serializable]
+    class Animation
+    {
+        public float duration;
+        [SerializeField] string _distEndSpeedCurveName;
+        [SerializeField] string _zEndSpeedCurveName;
 
-    [SerializeField] string _amplitudeCurveName;
-    [SerializeField] string _amplitudeSpeedCurveName;
+        [SerializeField] string _amplitudeCurveName;
+        [SerializeField] string _amplitudeSpeedCurveName;
+
+        [NonSerialized] public EvaluationCurve distEndSpeedCurve;
+        [NonSerialized] public EvaluationCurve zEndSpeedCurve;
+        [NonSerialized] public EvaluationCurve amplitudeCurve;
+        [NonSerialized] public EvaluationCurve amplitudeSpeedCurve;
+
+        public void Init()
+        {
+            if (distEndSpeedCurve == null)
+            {
+                distEndSpeedCurve = EvaluationCurveManager.instance.GetCurve(_distEndSpeedCurveName);
+                zEndSpeedCurve = EvaluationCurveManager.instance.GetCurve(_zEndSpeedCurveName);
+                amplitudeCurve = EvaluationCurveManager.instance.GetCurve(_amplitudeCurveName);
+                amplitudeSpeedCurve = EvaluationCurveManager.instance.GetCurve(_amplitudeSpeedCurveName);
+            }
+        }
+    }
+
+    [Header("Animation")]
+    [SerializeField] Animation _landAnimation;
+
+    //[SerializeField] float _landDuration;
+    //[SerializeField] string _distEndSpeedCurveName;
+    //[SerializeField] string _zEndSpeedCurveName;
+
+    //[SerializeField] string _amplitudeCurveName;
+    //[SerializeField] string _amplitudeSpeedCurveName;
 
     [Header("Line Settings")]
     [SerializeField] int _pointCount;
@@ -60,13 +89,15 @@ public class FishingLine : MonoBehaviour
         _to = to;
         _time = 0f;
 
-        if (_distEndSpeedCurve == null)
-        {
-            _distEndSpeedCurve = EvaluationCurveManager.instance.GetCurve(_distEndSpeedCurveName);
-            _zEndSpeedCurve = EvaluationCurveManager.instance.GetCurve(_zEndSpeedCurveName);
-            _amplitudeCurve = EvaluationCurveManager.instance.GetCurve(_amplitudeCurveName);
-            _amplitudeSpeedCurve = EvaluationCurveManager.instance.GetCurve(_amplitudeSpeedCurveName);
-        }
+        _landAnimation.Init();
+
+        //if (_distEndSpeedCurve == null)
+        //{
+        //    _distEndSpeedCurve = EvaluationCurveManager.instance.GetCurve(_distEndSpeedCurveName);
+        //    _zEndSpeedCurve = EvaluationCurveManager.instance.GetCurve(_zEndSpeedCurveName);
+        //    _amplitudeCurve = EvaluationCurveManager.instance.GetCurve(_amplitudeCurveName);
+        //    _amplitudeSpeedCurve = EvaluationCurveManager.instance.GetCurve(_amplitudeSpeedCurveName);
+        //}
 
         _p0 = from;
 
@@ -91,10 +122,10 @@ public class FishingLine : MonoBehaviour
 
         if(_isLanding)
         {
-            if(_time > _duration)
+            if(_time > _landAnimation.duration)
             {
                 _isLanding = false;
-                _time = _duration;
+                _time = _landAnimation.duration;
             }
 
             RefreshControlPoints(_time);
@@ -102,7 +133,7 @@ public class FishingLine : MonoBehaviour
         }
         else if(_isLanded)
         {
-            RefreshControlPoints(_duration);
+            RefreshControlPoints(_landAnimation.duration);
             RefreshLinePoints();
         }
     }
@@ -111,18 +142,18 @@ public class FishingLine : MonoBehaviour
     {
         _p0 = from;
 
-        float distance01 = _distEndSpeedCurve.Evaluate(time);
+        float distance01 = _landAnimation.distEndSpeedCurve.Evaluate(time);
         Vector2 pos2D = (_to - from) * distance01 + from;
 
-        float z01 = _zEndSpeedCurve.Evaluate(time);
+        float z01 = _landAnimation.zEndSpeedCurve.Evaluate(time);
         float z = (_to.z - from.z) * z01 + from.z;
 
         _p2 = new Vector3(pos2D.x, pos2D.y, z);
 
-        distance01 = _amplitudeSpeedCurve.Evaluate(time);
+        distance01 = _landAnimation.amplitudeSpeedCurve.Evaluate(time);
         pos2D = (_p2 - from) * distance01 + from;
 
-        z01 = _amplitudeCurve.Evaluate(time);
+        z01 = _landAnimation.amplitudeCurve.Evaluate(time);
         z = (_p2.z - from.z) * z01 + from.z;
 
         _p1 = new Vector3(pos2D.x, pos2D.y, z);
