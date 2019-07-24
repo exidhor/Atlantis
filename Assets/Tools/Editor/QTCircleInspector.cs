ba633ba8e4268df48bef824d6c53e87a67cc55f8
Exhidor <jipay.detiege@gmail.com>
2019-07-24 14:17:26 +0200

    add collider circle

---
+++ Assets/Tools/Editor/QTCircleInspector.cs
@@ -0,0 +1,80 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ToolsEditor
+{
+    [CustomEditor(typeof(Tools.QTCircle)), CanEditMultipleObjects]
+    public class QTCircleInspector : Editor
+    {
+        readonly float offsetY = 0f;
+
+        Tool LastTool = Tool.None;
+
+        void OnEnable()
+        {
+            LastTool = UnityEditor.Tools.current;
+        }
+
+        void OnDisable()
+        {
+            UnityEditor.Tools.current = LastTool;
+        }
+
+
+        private void OnSceneGUI()
+        {
+            Tools.QTCircle circle = target as Tools.QTCircle;
+
+            //Transform handleTransform = circle.transform;
+            //Quaternion handleRotation = handleTransform.rotation;
+            //Vector3 p0 = handleTransform.TransformPoint(circle.p0);
+            //Vector3 p1 = handleTransform.TransformPoint(circle.p1);
+
+            //Handles.color = Color.white;
+            //Handles.DrawLine(p0, p1);
+            //Handles. DoPositionHandle(p0, handleRotation);
+            //Handles.DoPositionHandle(p1, handleRotation);
+
+            Handles.color = Color.green;
+
+            Vector3 center = Tools.WorldConversion.ToVector3(circle.center);
+
+            Handles.DrawWireDisc(center,
+                                 Vector3.up, 
+                                 circle.radius);
+
+            Handles.color = Color.white;
+
+            if(!circle.edit)
+            {
+                UnityEditor.Tools.current = LastTool;
+                return;
+            }
+
+            UnityEditor.Tools.current = Tool.None;
+
+            EditorGUI.BeginChangeCheck();
+            float radius = Handles.RadiusHandle(Quaternion.identity,
+                                                center,
+                                                circle.radius,
+                                                true);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(circle, "Change Radius Circle Collider");
+                circle.SetRadius(radius);
+            }
+
+            EditorGUI.BeginChangeCheck();
+            Vector3 newCenter = Handles.PositionHandle(center + new Vector3(0f, offsetY, 0f), 
+                                                       Quaternion.identity);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(circle, "Change Offset Circle Collider");
+                Vector2 offset = Tools.WorldConversion.ToVector2(newCenter 
+                                                                 - circle.transform.position
+                                                                 - new Vector3(0f, offsetY, 0f));
+                circle.SetOffset(offset);
+            }
+        }
+    }
+}
---
+++ Assets/Tools/Scripts/QuadTree/Circular/QTCircle.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Tools
+{
+    public class QTCircle : MonoBehaviour, IQTClearable
+    {
+#if UNITY_EDITOR
+        public bool edit
+        {
+            get { return _edit; }
+        }
+
+        [SerializeField] bool _edit;
+#endif
+
+        [SerializeField] Vector2 _offset;
+        [SerializeField] float _radius;
+        [SerializeField] bool _persistent;
+
+        public bool isEnable
+        {
+            get { return enabled; }
+        }
+
+        public bool persistent
+        {
+            get { return _persistent; }
+        }
+
+        public Vector2 center
+        {
+            get 
+            {
+                return WorldConversion.ToVector2(transform.position)
+                       + _offset;
+                }
+        }
+
+        public float radius
+        {
+            get { return _radius; }
+        }
+
+        public Rect GetGlobalBounds()
+        {
+            return new Rect(center - Vector2.one * radius,
+                            Vector2.one * 2 * radius);
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            //Gizmos.color = Color.green;
+
+            //Vector3 center = transform.position 
+            //                 + WorldConversion.ToVector3(_offset);
+            //center.y = 0;
+
+            //Gizmos.DrawWireSphere(center, _radius);
+
+            Gizmos.color = Color.red;
+
+            Rect rect = GetGlobalBounds();
+
+            Gizmos.DrawWireCube(WorldConversion.ToVector3(rect.center),
+                                WorldConversion.ToVector3(rect.size));
+        }
+
+        public void SetRadius(float radius)
+        {
+            _radius = radius;
+        }
+
+        public void SetOffset(Vector2 offset)
+        {
+            _offset = offset;
+        }
+    }
+}
