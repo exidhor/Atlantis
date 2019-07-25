b9a415987c6026a257e862dec5e21f248f17caf8
Exhidor <jipay.detiege@gmail.com>
2019-07-25 17:11:19 +0200

    add fish library

---
+++ Assets/Atlantis/Scripts/Fishing/FishInfo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using Tools;
+
+[Serializable]
+public class FishInfo
+{
+    public FishType type
+    {
+        get { return _type; }
+    }
+
+    public Sprite icon
+    {
+        get { return _icon; }
+    }
+
+    [SerializeField] FishType _type;
+    [SerializeField] Sprite _icon;
+    [SerializeField] Vector2i _questRange;
+    [SerializeField] Vector2i _oneFishPriceRange;
+}
--- Assets/Atlantis/Scripts/Fishing/FishLibrary.cs
+++ Assets/Atlantis/Scripts/Fishing/FishLibrary.cs
@@ -1,8 +1,19 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System;
+using System.Collections.Generic;
 using Tools;
 
 public class FishLibrary : MonoSingleton<FishLibrary>
 {
-    
+    [SerializeField] List<FishInfo> _fishes = new List<FishInfo>();
+
+    void Awake()
+    {
+        _fishes.Sort((a, b) => a.type.CompareTo(b.type));
+    }
+
+    public FishInfo GetFishInfo(FishType type)
+    {
+        return _fishes[(int)type];
+    }
 }
