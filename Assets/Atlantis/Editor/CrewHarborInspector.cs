9fa9072dc460d4cb0662b792e149f8e34cb3a474
Exhidor <jipay.detiege@gmail.com>
2019-08-01 16:06:14 +0200

    add inspector for harbors

---
+++ Assets/Atlantis/Editor/CrewHarborInspector.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEditor;
+using ToolsEditor;
+
+[CustomEditor(typeof(CrewHarbor)), CanEditMultipleObjects]
+public class CrewHarborInspector : QTCircleInspector
+{
+
+}
---
+++ Assets/Atlantis/Editor/FishHarborInspector.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEditor;
+using ToolsEditor;
+
+[CustomEditor(typeof(FishHarbor)), CanEditMultipleObjects]
+public class FishHarborInspector : QTCircleInspector
+{
+
+}
